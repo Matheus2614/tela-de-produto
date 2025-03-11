c4947e9bf3cb978feb17b8812ac12c72b33c694b
Matheus WIncler Santos <165319984+Matheus2614@users.noreply.github.com>
2025-03-11 12:15:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function mudarfoto(item) {
+
+    if (item == 1) {
+        document.getElementById("icone").src = "img/frente.png";
+
+    } else if (item == 2) {
+        document.getElementById("icone").src = "img/lado.png";
+
+    } else if (item == 3) {
+        document.getElementById("icone").src = "img/tras.png";
+
+    };
+};
